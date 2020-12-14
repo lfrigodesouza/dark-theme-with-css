e57d2fffd165f4a4142d30d2d088ac239191d86e
Lucas Frigo de Souza <lfrigodesouza@outlook.com>
2020-12-14 14:21:54 -0300

    Finaliza página de demonstração

--- main.js
+++ main.js
@@ -1,23 +1,33 @@
-'use strict'
-let browserTheme = (window.matchMedia("(prefers-color-scheme: light)").matches ? "light" : "dark")
-document.documentElement.setAttribute('data-theme', browserTheme)
-
-let themeToggler = document.getElementById("theme-toggler");
-themeToggler.addEventListener('click', ()=>{
-    console.log("trocando o tema")
-    let targetTheme;
-    let currentTheme = document.documentElement.getAttribute("data-theme");
-    if (currentTheme === "light") {
-        targetTheme = "dark";
-    } else {
-        targetTheme = "light"
-    }
-    document.documentElement.setAttribute('data-theme', targetTheme)
-})
-
-let count = 0;
-function handleButton(){
-    count += 1;
-    document.getElementById("count-label").innerHTML = `Vezes clicadas: ${count}`
+let savedTheme = localStorage.getItem('theme');
+let saveTheme = document.getElementById("save-theme");
+if(savedTheme){
+    document.documentElement.setAttribute('data-theme', savedTheme)
+    saveTheme.checked = true;
+}else {
+    let browserTheme = (window.matchMedia('(prefers-color-scheme: light)').matches ? 'light' : 'dark')
+    document.documentElement.setAttribute('data-theme', browserTheme)
 }
 
+saveTheme.addEventListener('change', (evt) => {
+    if(evt.target.checked){
+        let currentTheme = document.documentElement.getAttribute("data-theme")
+        localStorage.setItem('theme', currentTheme);    
+    }else{
+        localStorage.removeItem('theme');
+    }
+});
+
+let themeToggler = document.getElementById('theme-toggler');
+themeToggler.addEventListener('click', ()=>{
+    let targetTheme;
+    let currentTheme = document.documentElement.getAttribute('data-theme');
+    if (currentTheme === 'light') {
+        targetTheme = 'dark';
+    } else {
+        targetTheme = 'light'
+    }
+    if(saveTheme.checked) {
+        localStorage.setItem('theme', targetTheme);
+    }
+    document.documentElement.setAttribute('data-theme', targetTheme)
+});
