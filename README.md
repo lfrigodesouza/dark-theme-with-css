490942153cf08090b32020d1fd1f6556f0400458
Lucas Frigo de Souza <lfrigodesouza@outlook.com>
2020-12-17 18:37:50 -0300

    Corrige url do artigo e melhora formatação

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-Página de demonstração para o artigo [Criando um tema escuro com JS e CSS](https://www.lfrigodesouza/2020/12/16/Criando-um-tema-escuro-com-JS-e-CSS/)
+Página de demonstração para o artigo [Criando um tema escuro com JS e CSS](https://lfrigodesouza.net/2020/12/17/Criando-um-tema-escuro-com-JS-e-CSS/?utm_medium=source_code&utm_source=github)
 
 Essa página pode ser vizualizada em [https://lfrigodesouza.github.io/dark-theme-with-css/](https://lfrigodesouza.github.io/dark-theme-with-css/)
--- main.js
+++ main.js
@@ -1,33 +1,35 @@
-let savedTheme = localStorage.getItem('theme');
+let savedTheme = localStorage.getItem("theme");
 let saveTheme = document.getElementById("save-theme");
-if(savedTheme){
-    document.documentElement.setAttribute('data-theme', savedTheme)
-    saveTheme.checked = true;
-}else {
-    let browserTheme = (window.matchMedia('(prefers-color-scheme: light)').matches ? 'light' : 'dark')
-    document.documentElement.setAttribute('data-theme', browserTheme)
+if (savedTheme) {
+  document.documentElement.setAttribute("data-theme", savedTheme);
+  saveTheme.checked = true;
+} else {
+  let browserTheme = window.matchMedia("(prefers-color-scheme: light)").matches
+    ? "light"
+    : "dark";
+  document.documentElement.setAttribute("data-theme", browserTheme);
 }
 
-saveTheme.addEventListener('change', (evt) => {
-    if(evt.target.checked){
-        let currentTheme = document.documentElement.getAttribute("data-theme")
-        localStorage.setItem('theme', currentTheme);    
-    }else{
-        localStorage.removeItem('theme');
-    }
+saveTheme.addEventListener("change", (evt) => {
+  if (evt.target.checked) {
+    let currentTheme = document.documentElement.getAttribute("data-theme");
+    localStorage.setItem("theme", currentTheme);
+  } else {
+    localStorage.removeItem("theme");
+  }
 });
 
-let themeToggler = document.getElementById('theme-toggler');
-themeToggler.addEventListener('click', ()=>{
-    let targetTheme;
-    let currentTheme = document.documentElement.getAttribute('data-theme');
-    if (currentTheme === 'light') {
-        targetTheme = 'dark';
-    } else {
-        targetTheme = 'light'
-    }
-    if(saveTheme.checked) {
-        localStorage.setItem('theme', targetTheme);
-    }
-    document.documentElement.setAttribute('data-theme', targetTheme)
+let themeToggler = document.getElementById("theme-toggler");
+themeToggler.addEventListener("click", () => {
+  let targetTheme;
+  let currentTheme = document.documentElement.getAttribute("data-theme");
+  if (currentTheme === "light") {
+    targetTheme = "dark";
+  } else {
+    targetTheme = "light";
+  }
+  if (saveTheme.checked) {
+    localStorage.setItem("theme", targetTheme);
+  }
+  document.documentElement.setAttribute("data-theme", targetTheme);
 });
